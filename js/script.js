4abe1f7f5a1c806d115217cf707ada020f27ea06
AlejandroRdz00 <24040042@alumno.utc.edu.mx>
2025-09-28 23:53:52 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+ function getValues() {
+      let n1 = parseFloat(document.getElementById("num1").value);
+      let n2 = parseFloat(document.getElementById("num2").value);
+      return [n1, n2];
+    }
+
+    function sumar() {
+      let [n1, n2] = getValues();
+      alert("Resultado: " + (n1 + n2));
+    }
+
+    function restar() {
+      let [n1, n2] = getValues();
+      alert("Resultado: " + (n1 - n2));
+    }
+
+    function multiplicar() {
+      let [n1, n2] = getValues();
+      alert("Resultado: " + (n1 * n2));
+     
+    }
+
+    function dividir() {
+      let [n1, n2] = getValues();
+      if (n2 === 0) {
+        alert("Error: No se puede dividir entre 0");
+      } else {
+        alert("Resultado: " + (n1 / n2));
+      }
+    }
